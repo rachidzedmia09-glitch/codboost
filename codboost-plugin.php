d67b4b843392f2a80a3a8f6fbd2c1faaa62932ba
rachidzedmia09-glitch <rachidzedmia09@gmail.com>
2025-10-08 13:27:04 +0100

    Update codboost-plugin.php

--- codboost-plugin.php
+++ codboost-plugin.php
@@ -1,1 +1,10 @@
-
+    <?php
+    /*
+    Plugin Name: Codboost Plugin
+    Plugin URI: https://codboost.pro/
+    Description: A brief description of my custom WordPress plugin.
+    Version: 1.0.0
+    Author: Your Name
+    Author URI: https://codboost.pro/
+    License: GPL2
+    */
